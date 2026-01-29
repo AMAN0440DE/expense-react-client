05adc6f807e7e3ecc7b66c1615f78966c36ad423
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-29 10:39:00 +0530

    React Router DOM/ wrapped BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />   
+    </BrowserRouter>
   </StrictMode>,
 )
+// wrapped the entire app inside <BrowserRouter>
+// entire vs code is built using electron.js
