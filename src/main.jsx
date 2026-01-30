250e2093169d7a4476fd95bd7d425b696580590a
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-30 23:55:49 +0530

    createRoot()

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import App from './App.jsx'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-import {BrowserRouter} from "react-router-dom";
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter> 
-      <App />   
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
-// wrapped the entire app inside <BrowserRouter>
-// entire vs code is built using electron.js
+  </StrictMode>
+);
+
