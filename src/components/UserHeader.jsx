75af1b5cab3367ec89824145d181a769d77e345f
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-31 00:02:33 +0530

    Added nav item list for Register

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -33,6 +33,12 @@ function UserHeader() {
                 Login
               </Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </li>
+
           </ul>
         </div>
       </div>
