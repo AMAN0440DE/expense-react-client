b2aa496c1b992af5362e6b24eb993d19ff9f4c84
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-30 22:27:56 +0530

    Modified UserFooter.jsx

---
+++ src/components/UserFooter.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+function Footer(){
+    return(
+        <div className="container">
+            This is Footer
+
+        </div>
+    );
+}
+export default Footer;
