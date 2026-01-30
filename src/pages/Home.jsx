3dd68d56841d0593dfe5b8dbe01a7963f24f1211
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-30 23:50:08 +0530

    Welcome to Expense App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="container">
+      <h3 className="text-center">Welcome to Expense App</h3>
+    </div>
+  );
+}
+
+// we can create generic components for the components 
+// that are not changing throughout the website like header, footer
+
+export default Home;
