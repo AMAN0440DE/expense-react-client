6eb3b6eff54e31f8633a5e2e285d617eee259e09
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-31 23:21:18 +0530

    added some theory about responsive

---
+++ theory.js
@@ -0,0 +1,35 @@
+/**
+ * push everything except node modules and packagelock.json on github
+ * 
+ * clean the files
+ * delete publuic files, App.jsx- all components except stated, main.jsx - alll css
+ * 
+ * //////////////////////////
+ * 
+ * REACT has hooks
+ * like useState
+ * anything that start with use is a hook
+ * hook is used to compare withDOM
+ * 
+ * useStat
+ * ----------
+ * - variable
+ * - return array with two value- array itself, function to change the value
+ * - const[variable, function] = useState(default_value)
+ * 
+ * ////////////////////
+ * RESPONSIVE web design
+ * usually divides the screen into 12 columns- because 12 is mostly divisible by many no. below it
+ * eg. bootstrap provides col class  - <div class "col" col-sm -12 col-md - 6 col-xl -2">
+ * 
+ * 
+ * CDN content delivery network - collection of servers delivers the web contevnt and whichever server is nearest to client will be delivered and by this way 
+ * it will be faster, CDN lives in different parts of the world when one loads the jS files it also downloads CDN files
+ * 
+ * integrate bootstrap using CDN
+ * 
+ * and components in bootstrap are in html and react uses jsx so need to converted to jsx from converter
+ * // use the link to cdn in index.html
+ * 
+ * 
+ */
