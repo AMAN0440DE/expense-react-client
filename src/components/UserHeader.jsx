63d8b50e587d149e69fa545bb699a5431108fbfe
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-31 23:17:48 +0530

    UserHeader added

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,49 +1,49 @@
 import { Link } from "react-router-dom";
 
+// import { Link } from "react-router-dom";
+
 function UserHeader() {
-  return (
-    // Added 'navbar', 'navbar-expand-lg', and background colors
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          ExpenseApp
-        </Link>
-        
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home 
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-            </li>
-
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/dashboard">
+                    ExpenseApp
+                </Link>
+                <button
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    className="navbar-toggler"
+                    data-bs-target="#navbarSupportedContent"
+                    data-bs-toggle="collapse"
+                    type="button"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link
+                                aria-current="page"
+                                className="nav-link active"
+                                to="/dashboard"
+                            >
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/logout">
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default UserHeader;
