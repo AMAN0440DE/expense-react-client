d9803c77c52011ef3cf92ec33c21f2ede7c8b30c
AMAN0440DE <kumar02aman2004@gmail.com>
2026-02-04 23:52:26 +0530

    UserHeader updated for My groups

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 // import { Link } from "react-router-dom";
 
 function UserHeader() {
@@ -24,7 +24,12 @@ function UserHeader() {
                     className="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/groups">
+                                My Groups
+                            </Link>
+                        </li>
                         <li className="nav-item">
                             <Link
                                 aria-current="page"
